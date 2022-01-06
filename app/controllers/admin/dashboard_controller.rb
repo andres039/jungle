0ab67c7e9803edb5618aadd3f2be0a516d39c606
Andres <andres039039@Hotmail.com>
2022-01-06 23:36:25 +0000

    adds filters for login/signup functionalities

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,5 @@
 class Admin::DashboardController < ApplicationController
+  before_filter :authorize
   def show
     @products_count = Product.all.length
     @categories_count = Category.all.length
