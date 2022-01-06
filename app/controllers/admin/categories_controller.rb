4a77ade1814f9e51bbb5046129e3da220d48eb55
Andres <andres039039@Hotmail.com>
2022-01-06 20:20:23 +0000

    fixes about page functionality

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,29 @@
+class Admin::CategoriesController < ApplicationController
+  def index
+    @categories = Category.order(id: :desc).all
+  end
+
+  def new
+    @categories= Category.new
+  end
+
+  def create
+    @categories= Category.new(categories_params)
+
+    if @categories.save
+      redirect_to [:admin, :categories], notice: 'Category created!'
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def categories_params
+    params.require(:category).permit(
+      :name,
+      :created_at,
+      :updated_at
+    )
+  end
+end
