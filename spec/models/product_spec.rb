15710c10e73d6c41bc8364c9b8fe1fefb9fe9f41
Andres <andres039039@Hotmail.com>
2022-01-10 23:33:07 +0000

    Implements five examples for the product_spec

---
+++ spec/models/product_spec.rb
@@ -0,0 +1,36 @@
+require 'rails_helper'
+require 'product.rb'
+RSpec.describe Product, type: :model do
+ describe 'Validations' do
+    # validation tests/examples here
+  before (:each) do
+   @category = Category.new(name: 'products')
+   @product = Product.new(name: 'donuts', price_cents: 25, quantity: 4, category:  @category)
+  end
+
+ it "has a all fields set" do
+  expect(@product).to be_valid
+ end
+
+ it "doesn't have a name" do
+  @product.name = nil
+  expect(@product).to_not be_valid
+ end
+
+ it "doesn't have a price" do
+  @product.price_cents = nil 
+  expect(@product).to_not be_valid
+ end
+
+ it "doesn't define a quantity" do
+  @product.quantity = nil
+  expect(@product).to_not be_valid
+ end
+ 
+ it "doesn't belong to a category" do
+  @product.category = nil
+  expect(@product).to_not be_valid
+ end
+
+ end
+end 
