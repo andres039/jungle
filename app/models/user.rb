85e646d79f3a1e25e6d215e9bd051885527dfd16
Andres <andres039039@Hotmail.com>
2022-01-11 22:20:32 +0000

    initial setup

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,7 @@
 class User < ActiveRecord::Base
   has_secure_password
 
-  validates :first_name, presence: true
-  validates :last_name, presence: true
+  validates :name, presence: true
   validates :email, presence: true
   validates :password, presence: true
   validates :password_confirmation, presence: true
