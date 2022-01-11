fedf5ab7117c713c7d6240ae2b593d27998c3179
Andres <andres039039@Hotmail.com>
2022-01-11 15:41:40 +0000

    adds testing gems

--- db/schema.rb
+++ db/schema.rb
@@ -66,8 +66,7 @@ ActiveRecord::Schema.define(version: 20220107011108) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "first_name"
-    t.string   "last_name"
+    t.string   "name"
     t.string   "email"
     t.string   "password_digest"
     t.datetime "created_at",      null: false
