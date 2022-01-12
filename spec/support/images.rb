5e8402a27393593a2515fbfe07764ddadab679b9
Andres <andres039039@Hotmail.com>
2022-01-12 00:53:47 +0000

    fix images.rb

--- spec/support/images.rb
+++ spec/support/images.rb
@@ -1,7 +1,4 @@
-<<<<<<< HEAD
 # Helper function to open product images
-=======
->>>>>>> feature/testing-product-page
 def open_asset(file_name)
   File.open(Rails.root.join('db', 'seed_assets', file_name))
 end
